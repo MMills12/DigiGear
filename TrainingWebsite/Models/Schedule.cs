a0727d12ca2293b34b00c046d1f7e7cb6497bbb6
Michael Mills <michaelmills@quickenloans.com>
2018-05-06 16:48:02 -0400

    First Commit

---
+++ TrainingWebsite/Models/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrainingWebsite.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int SyllabusId { get; set; }
+        public int Week { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; }
+    }
+}
